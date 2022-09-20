c9812cd40869dc0acbc81b81efb420acf18dd397
Marko <jantulamarko@outlook.com>
2022-09-20 08:42:26 +0200

    Update Formatter.cs

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,11 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            Console.Write(Math.Round(numbers[0], 2) + " ");
+            Console.Write(Math.Round(numbers[1], 2) + " ");
+            Console.Write(Math.Round(numbers[2], 2) + " ");
+            Console.Write(Math.Round(numbers[3], 2) + " ");
+            Console.Write(Math.Round(numbers[4], 2));
         }
     }
 }
